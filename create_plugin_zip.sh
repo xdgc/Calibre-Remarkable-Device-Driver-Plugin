3c3f7b0626eede8edcf509e5366d7fb3846f503b
David Champion <dgc@c13.us>
2024-07-19 00:16:03 -0700

    Make create_plugin_zip.sh more resilient and correct

--- create_plugin_zip.sh
+++ create_plugin_zip.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 # Helper script to generate the plugin zip file which can be directly
 # installed by Calibre
 rm -rf ./calibre-remarkable-device-driver-plugin.zip ./target
 
-mkdir target
+mkdir -p target
 cp __init__.py config.py LICENSE plugin-import-name-remarkable_plugin.txt target/
-pushd ./target
-# Include all the dependencies in the zip archive
-pip install git+https://github.com/nick8325/remarkable-fs -t ./
-zip -r ../calibre-remarkable-device-driver-plugin.zip ./ -x ".*" "*.pyc"
-popd
+
+(
+	cd ./target
+
+	# Do this in case MacOS
+	PATH="/Applications/calibre.app/Contents/MacOS:$PATH"
+	PATH="$HOME/Applications/calibre.app/Contents/MacOS:$PATH"
+
+	# You need to use the same Python as Calibre itself uses.
+	pyver=$(calibre-debug -c 'from sys import version_info as v; print(f"{v.major}.{v.minor}");'||true)
+	[ -n "$pyver" ] || {
+		echo >&2 'You must have calibre-debug in your $PATH to build the plugin.'
+		exit 10
+	}
+
+	# Check for pip
+	pip${pyver} -V >/dev/null || {
+		echo >&2 "Calibre is using Python v$pyver, so to build this plugin you"
+		echo >&2 "need a Python $pyver runtime installed, with pip$pyver present."
+		exit 20
+	}
+
+	# Include all the dependencies in the zip archive
+	pip${pyver} install git+https://github.com/nick8325/remarkable-fs -t ./
+	zip -r ../calibre-remarkable-device-driver-plugin.zip ./ -x ".*" "*.pyc"
+)
